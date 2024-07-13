245bf594523b9ec404c267a637a2c16871a55b6b
Lora Reames <shes.lora.reames@gmail.com>
2024-07-12 23:03:20 -0500

    use tea.withAltScreen() instead of inline mode

--- main.go
+++ main.go
@@ -143,7 +143,7 @@ func (m *model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
